4045da45040ef0bc22e8c1535ffdb14fee676b2e
Thiago Tesla <thiagobarros95@gmail.com>
2022-07-21 17:12:26 -0300

    feat(:sparkles:): clean use case

--- src/Purchase.php
+++ src/Purchase.php
@@ -2,8 +2,6 @@
 
 namespace src;
 
-use Exception;
-
 class Purchase
 {
     private $products = [
@@ -23,30 +21,9 @@ class Purchase
         return $totalPurchase;
     }
 
-    public function totalPurchaseWithDiscount($category)
+    public function totalPurchaseWithDiscount(ICategoryDiscount $category)
     {
         $grossPricePurchase = $this->totalPurchase();
-
-        if ($category === 1) {
-            $netPricePurchase = $grossPricePurchase -= ($grossPricePurchase * 0.10);
-        } else if ($category === 2) {
-            $netPricePurchase = $grossPricePurchase -= ($grossPricePurchase * 0.20);
-        } else if ($category === 3) {
-            $netPricePurchase = $grossPricePurchase -= ($grossPricePurchase * 0.30);
-        } else if ($category === 4) {
-            $netPricePurchase = $grossPricePurchase -= ($grossPricePurchase * 0.40);
-        } else if ($category === 5) {
-            $netPricePurchase = $grossPricePurchase -= ($grossPricePurchase * 0.50);
-        }
-        /*
-            This implementation violates the open-closed principle as, 
-            with each new category added, a new else if must be created. 
-            This function is not closed for modification.
-            This function will never stop growing.
-        */
-        else {
-            throw new Exception("Invalid category.");
-        }
-        return $netPricePurchase;
+        return $category->netPricePurchase($grossPricePurchase);
     }
 }
